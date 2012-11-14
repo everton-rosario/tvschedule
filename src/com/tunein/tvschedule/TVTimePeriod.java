7c8f6801f27a5f0a9251df6dea54ab6c2bb7da0a
Everton Fernandes Rosario <erosario@gmail.com>
2012-11-13 22:11:44 -0200

    parsing and treating parsing errors/possibilities

--- src/com/tunein/tvschedule/TVTimePeriod.java
+++ src/com/tunein/tvschedule/TVTimePeriod.java
@@ -7,6 +7,7 @@ import java.text.DateFormatSymbols;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @author Everton Rosario (erosario@gmail.com)
@@ -15,6 +16,13 @@ public class TVTimePeriod extends TimePeriod {
     
     private static Map<String, Integer> WEEK_DAYS_LONG = new HashMap<String, Integer>();
     private static Map<String, Integer> WEEK_DAYS_SHORT = new HashMap<String, Integer>();
+    
+    private static Pattern DURATION_PATTERN_DEFAULT = Pattern.compile("\\d+hr");
+    private static Pattern DURATION_PATTERN_EXTRA_MIN = Pattern.compile("\\d+:\\d*hr");
+    private static Pattern HOUR24_PATTERN = Pattern.compile("\\d+:\\d+");
+    private static Pattern HOUR24_PATTERN_NOMINUTE = Pattern.compile("\\d+:\\d+");
+    private static Pattern HOUR_AMPM_PATTERN = Pattern.compile("\\d+:\\d+(am|pm)");
+    private static Pattern HOUR_AMPM_PATTERN_NOMINUTE = Pattern.compile("\\d+(am|pm)");
     
     // Maps intialization for parsing Week days
     static {
@@ -46,28 +54,84 @@ public class TVTimePeriod extends TimePeriod {
 
 
     private static long getTime(String weekDay, String startTime) {
-        Integer day = getDayOfWeek(weekDay);
-        Integer hour = getHourOfDay(startTime);
-        Integer min = getMinuteOfDay(startTime);
+        long day = getDayOfWeek(weekDay);
+        long daytime = getDaytime(startTime);
         
-        return day * 24 * 3600 * 1000 + // Millis for the day
-               hour * 3600 * 1000 +     // Millis for the hour
-               min * 60 * 1000;         // Millis for minute
+        return day * 24L * 3600L * 1000L + // Millis for the day
+               daytime;                    // Millis for the daytime
     }
 
+    
+    /**
+     * @param duration Expected format: 1hr | 1:30hr | 10hr
+     * @return millis format of hour + minute
+     */
     private static long getDuration(String duration) {
-        // TODO Auto-generated method stub
-        return 0l;
+        long hour = 0L;
+        long min = 0L;
+        
+        // Example: 20hr
+        if (DURATION_PATTERN_DEFAULT.matcher(duration).matches()) {
+            hour = Long.parseLong(duration.substring(0, duration.indexOf("hr")));
+        }
+
+        // Example: 10:30hr
+        else if (DURATION_PATTERN_EXTRA_MIN.matcher(duration).matches()) {
+            hour = Long.parseLong(duration.substring(0, duration.indexOf(":")));
+            min  = Long.parseLong(duration.substring(duration.indexOf(":") + 1, duration.indexOf("hr")));
+        }
+        
+        // Example: 10:30
+        else if (HOUR24_PATTERN.matcher(duration).matches()) {
+            hour = Long.parseLong(duration.substring(0, duration.indexOf(":")));
+            min  = Long.parseLong(duration.substring(duration.indexOf(":") + 1));
+        }
+        
+        else {
+            throw new IllegalArgumentException("Invalid duration format: " + duration);
+        }
+
+        return hour * 3600L * 1000L + 
+                min * 60L * 1000L;
     }
 
-    private static Integer getMinuteOfDay(String startTime2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    private static long getDaytime(String startTime) {
+        long hour = 0L;
+        long min = 0L;
 
-    private static Integer getHourOfDay(String startTime2) {
-        // TODO Auto-generated method stub
-        return null;
+        // Example: 1pm
+        if (HOUR_AMPM_PATTERN_NOMINUTE.matcher(startTime).matches()) {
+            if (startTime.contains("am")) { 
+                hour = Long.parseLong(startTime.substring(0, startTime.indexOf("am")));
+            } else {
+                hour = 12 + Long.parseLong(startTime.substring(0, startTime.indexOf("pm")));
+            }
+        }
+
+        // Example: 1:30pm
+        else if (HOUR_AMPM_PATTERN.matcher(startTime).matches()) {
+            if (startTime.contains("am")) { 
+                hour = Long.parseLong(startTime.substring(0, startTime.indexOf(":")));
+                min = Long.parseLong(startTime.substring(startTime.indexOf(":") + 1, startTime.indexOf("am")));
+            } else {
+                hour = 12 + Long.parseLong(startTime.substring(0, startTime.indexOf(":")));
+                min = Long.parseLong(startTime.substring(startTime.indexOf(":") + 1, startTime.indexOf("pm")));
+            }
+        }
+
+        // Example: 10:30
+        else if (HOUR24_PATTERN.matcher(startTime).matches()) {
+            hour = Long.parseLong(startTime.substring(0, startTime.indexOf(":")));
+            min  = Long.parseLong(startTime.substring(startTime.indexOf(":") + 1));
+        } 
+        
+        // Example: 13
+        else if (HOUR24_PATTERN_NOMINUTE.matcher(startTime).matches()) {
+            hour = Long.parseLong(startTime);
+        }
+        
+        return hour * 3600L * 1000L + //millis in the hour
+                min * 60L * 1000L;    // millis in the minute
     }
 
     private static Integer getDayOfWeek(String weekDay) {
@@ -83,6 +147,9 @@ public class TVTimePeriod extends TimePeriod {
     
     public static void main(String[] args) {
         System.out.println(TVTimePeriod.getDayOfWeek("Sat"));
+        System.out.println(TVTimePeriod.getDuration("10hr"));
+        System.out.println(TVTimePeriod.getDuration("11:30hr"));
+        System.out.println(TVTimePeriod.getDuration("20:30"));
     }
 
 }
